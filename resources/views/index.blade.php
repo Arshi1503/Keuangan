4e7a9fc9d7c43fc6c7da91dadb8e54a395f29e90
Arshi <fahrezzi@widianto.org>
2024-11-26 20:17:09 +0700

    New: Membuat desain login/dashboard

---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" class="">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+    html {
+    /* height: 100%;
+    width: 100%;
+    background-image: url('{{ asset('img/Cash.jpg') }}');
+    background-repeat: no-repeat;
+    background-size: cover; /* atau gunakan 100% 100% */
+    background-position: center;
+    } */
+    </style>
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="bg-gradient-to-r from-[#f4f269] to-[#5cb270]">
+    <div class="h-screen flex items-center justify-center">
+        <div class="flex w-1/3 m-auto border-yellow-200 rounded-xl h-96 bg-yellow-200">
+            <div class="w-1/2 h-9 mx-auto my-auto rounded-xl bg-black"></div>
+        </div>
+    </div>
+</body>
+</html>
